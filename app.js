68193bd1fe007c5134b23fbbc3f516e099e09aa0
Syed Abdul Hannan <syedhannan546@gmail.com>
2021-11-11 18:46:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
